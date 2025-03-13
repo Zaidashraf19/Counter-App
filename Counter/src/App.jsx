4a38b2902e3ab700f8ca635aaf9ce1c0683ab61e
Zaidashraf19 <muhammadzaidmuhammadashraf@gmail.com>
2025-03-13 23:50:02 +0500

    Counter completed

---
+++ Counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function App(){
+    let [num , setNum] = useState(0)
+    function increment(){
+        setNum(num + 1)
+        console.log(num);
+    }
+    function decrement(){
+        if(num <= 0 ){
+            alert('NOT POSSIBLE ❗❗')
+        }else{
+            setNum(num - 1);
+            console.log(num);
+        }
+    }
+    return(
+        <>
+        <div><h1>COUNTER APP</h1></div>
+        <div style={
+            {display: 'flex',
+            justifyContent: "space-around",
+            fontFamily: 'revert-layer'
+            }}>
+                <button onClick={decrement}>-</button>
+                <p>{num}</p>
+                <button onClick={increment}>+</button>
+        </div>
+        </>
+    )
+}
+
+export default App
